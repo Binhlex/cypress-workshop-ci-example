10abcdacb9d6f358f3a16f9e9db8b6caafcb9e3f
Binhlex <binh.lexuan@nashtechglobal.com>
2022-01-26 17:18:02 +0700

    adjust to fail test

--- cypress/integration/spec1.js
+++ cypress/integration/spec1.js
@@ -3,6 +3,6 @@
 describe('Example site', () => {
   it('loads', { tags: 'circleci' }, () => {
     cy.visit('/')
-    cy.contains('h1', 'cypress-workshop-ci-example').should('be.visible')
+    cy.contains('h1', 'text-workshop-ci-example').should('be.visible')
   })
 })
